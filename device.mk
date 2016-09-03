a6079a8a0163254a5b22a45f30154385f691c033
Shreesha Murthy <shreeshu10@gmail.com>
2016-09-03 03:33:28 +0000

    oneplus2: Delete dhcpcd init entries, since dhcpcd is no more.

--- device.mk
+++ device.mk
@@ -128,8 +128,8 @@ PRODUCT_PACKAGES += \
     libtinyxml
 
 # Doze mode
-PRODUCT_PACKAGES += \
-    OneplusDoze
+#PRODUCT_PACKAGES += \
+#    OneplusDoze
 
 # Filesystem management tools
 PRODUCT_PACKAGES += \
@@ -278,7 +278,6 @@ PRODUCT_PACKAGES += \
     libQWiFiSoftApCfg \
     libwpa_client \
     hostapd \
-    dhcpcd.conf \
     wpa_supplicant \
     wpa_supplicant.conf \
     wpa_supplicant_overlay.conf \
